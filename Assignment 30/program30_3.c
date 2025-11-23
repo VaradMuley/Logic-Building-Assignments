10af285ea5063387fb49347f13ea00963dc31646
VaradMuley <varadmuley7333@gmail.com>
2025-11-23 22:24:49 +0530

    Add files via upload

---
+++ Assignment 30/program30_3.c
@@ -0,0 +1,76 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     program30_3.c
+//  Description :   Accept number from user and display below pattern
+//  Input :         Integer
+//  Output :        Integer
+//  Author :        Varad Nitin Muley
+//  Date :          21/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////
+// 
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+#include <stdio.h>
+
+
+void Display(int iRow,int iCol)
+{
+   int i = 0 ,j = 0;
+
+    for (i = 1 ; i <= iRow ; i++)
+    {
+        for (j = 1; j <= iCol; j++) 
+        {
+            if(j == i)
+            {
+                printf( "$\t");
+            }
+            else
+            {
+                printf( "#\t");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
+int main ()
+{
+    int iValue1 = 0 , iValue2 = 0 ;
+
+    printf("Enter the number of rows :\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of columns :\n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+
+    return 0 ;
+
+}
+
+
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases succesfully handaled by the application
+//  
+//  Input :       iRow = 4  iCol = 4
+//
+//  Output :     $ * * *       
+//               * $ * *    
+//               * * $ *
+//               * * * $
+//
+//////////////////////////////////////////////////////////////////
+
+
+
---
+++ Assignment 30/program30_5.java
@@ -0,0 +1,79 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     program30_5.java
+//  Description :   Accept numberof rows and columns from user and display pattern 
+//  Author :        Varad Nitin Muley
+//  Date :          21/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////
+// 
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+import java.util.*;
+
+
+
+class Pattern
+{
+    public void Display(int iRow,int iCol)
+    {
+        int i = 0 ,j = 0;
+
+        for (i = 1 ; i <= iRow ; i++)
+        {
+            for (j = 1; j <= iCol ; j++) 
+            {
+                if((j == 1) || (j == iCol) || (i == iRow) || ( i== 1))
+                {
+                    System.out.print( j+"\t");
+                }
+                else
+                {
+                    System.out.print( "*\t");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println(); 
+    }
+}
+
+class program30_5
+{
+    public static void main (String A[])
+    {
+        Pattern pobj = new Pattern();
+
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0 , iValue2 = 0 ;
+
+        System.out.println("Enter the Number of Rows: ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the Number of columns  : ");
+        iValue2 = sobj.nextInt();
+
+        pobj.Display(iValue1 , iValue2);
+
+    }
+}
+
+
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases succesfully handaled by the application
+//  
+//  Input :       iRow = 4  iCol = 4
+//
+//  Output :     1 2 3 4        
+//               1 * * 4    
+//               1 * * 4
+//               1 2 3 4
+//
+//////////////////////////////////////////////////////////////////
