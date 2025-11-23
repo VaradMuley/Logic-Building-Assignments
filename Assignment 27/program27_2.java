899ce6f663d3b1d23c2de19c57ea2026ac1e3a2e
VaradMuley <varadmuley7333@gmail.com>
2025-11-23 22:23:39 +0530

    Add files via upload

---
+++ Assignment 27/program27_2.java
@@ -0,0 +1,74 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     program27_2.java
+//  Description :   Accept numberof rows and columns from user and display pattern 
+//  Author :        Varad Nitin Muley
+//  Date :          21/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////
+// 
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+import java.util.*;
+
+
+class Pattern
+{
+    public void Display(int iRow,int iCol)
+    {
+        int i = 0 ,j = 0;
+ 
+        for (i = 1 ; i <= iRow ; i++)
+        {
+            for (j = 1 ; j <= iCol ; j++)
+            {
+                System.out.print(j + "\t");
+            }
+            System.out.println();
+        }
+       
+        System.out.println(); 
+    }
+}
+
+
+class program27_2
+{
+    public static void main (String A[])
+    {
+        Pattern pobj = new Pattern();
+
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0 , iValue2 = 0 ;
+
+        System.out.println("Enter the Number of Rows: ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the Number of columns  : ");
+        iValue2 = sobj.nextInt();
+
+        pobj.Display(iValue1 , iValue2);
+
+    }
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases succesfully handaled by the application
+//  
+//  Input :       iRow = 4  iCol = 3
+//
+//  Output :      1 2 3 
+//                1 2 3
+//                1 2 3
+//                1 2 3
+//
+//////////////////////////////////////////////////////////////////
